d8d45f31e25a280c461202661493cec2cd68272f
Rares <rares1200@gmail.com>
2020-01-19 01:25:32 +0200

    Fixed toolbar problem

--- rappslib/src/main/java/ro/ganduraci/rappslib/base/BaseActivity.kt
+++ rappslib/src/main/java/ro/ganduraci/rappslib/base/BaseActivity.kt
@@ -23,10 +23,10 @@ open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
-
+        setNavigation()
     }
 
-    fun setupNavigation() {
+    fun setNavigation() {
         supportFragmentManager.addOnBackStackChangedListener {
             getTopFragment()?.let {
                 setUpNavigation(it)
